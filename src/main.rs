59ce80b649949e9e027654f557b140c341dbc963
Thomas WACHE <thomas@wache.fr>
2023-07-18 17:27:09 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,170 @@
+use clap::Parser;
+use std::{io, net::SocketAddr};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
+
+mod sap;
+use sap::Wire;
+
+use crate::sap::SapRouter;
+
+#[derive(Debug, Parser)]
+struct Options {
+    sap_router: SocketAddr,
+    #[clap(short, long, default_value_t = 1080)]
+    bind_port: u16,
+}
+
+fn map_nom_error(e: nom::Err<nom::error::VerboseError<&[u8]>>) -> io::Error {
+    io::Error::new(io::ErrorKind::InvalidData, format!("{e:x?}"))
+}
+
+async fn sap_connect(addr: SocketAddr, host: &str, port: u16) -> io::Result<TcpStream> {
+    const NI_VERSION: u8 = 40;
+
+    let mut stream = TcpStream::connect(&addr).await?;
+    let hops = vec![
+        addr.into(),
+        sap::SapRouterHop {
+            hostname: host.into(),
+            port: format!("{}", port).into(),
+            password: "".into(),
+        },
+    ];
+
+    let mut sap_buffer = sap::SapNi::default();
+    sap_buffer.set(&sap::SapRouter::Route {
+        ni_version: NI_VERSION,
+        talk_mode: sap::TalkMode::RawIo,
+        rest_nodes: 1,
+        current_position: 1,
+        hops,
+    });
+
+    stream.write_all(sap_buffer.get_data()).await?;
+
+    sap_buffer.extract_from_reader(&mut stream).await?;
+
+    match SapRouter::decode(sap_buffer.get_data())
+        .map_err(map_nom_error)?
+        .1
+    {
+        SapRouter::Pong => Ok(stream),
+        SapRouter::ErrorInformation {
+            return_code, text, ..
+        } => {
+            let msg = if text.is_empty() {
+                format!("return code: {return_code}")
+            } else {
+                text.into()
+            };
+            Err(io::Error::new(io::ErrorKind::Other, msg))
+        }
+        r => Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!("Unexpected SapRouter variant: {r:?}"),
+        )),
+    }
+}
+
+async fn handle_client(sap_router: SocketAddr, mut stream: TcpStream) -> io::Result<()> {
+    let mut buffer = Vec::new();
+
+    let n = stream.read_buf(&mut buffer).await?;
+
+    let (_, hello) =
+        socks_parser::decode_socks_request_hello(&buffer[..n]).map_err(map_nom_error)?;
+    if hello
+        .methods
+        .contains(&socks_parser::AuthenticationMethod::None)
+    {
+        let response = socks_parser::response::Hello {
+            version: socks_parser::Version::Socks5,
+            method: socks_parser::AuthenticationMethod::None,
+        };
+        buffer.clear();
+        socks_parser::encode_socks_response_hello(&response, &mut buffer);
+        stream.write_all(&buffer[..]).await?;
+    } else {
+        let response = socks_parser::response::Hello {
+            version: socks_parser::Version::Socks5,
+            method: socks_parser::AuthenticationMethod::None,
+        };
+        socks_parser::encode_socks_response_hello(&response, &mut buffer);
+        stream.write_all(&buffer[..]).await?;
+        return Ok(());
+    }
+
+    buffer.clear();
+    let n = stream.read_buf(&mut buffer).await?;
+
+    let (_, request) = socks_parser::decode_socks_request(&buffer[..n]).map_err(map_nom_error)?;
+    if request.command != socks_parser::Command::Connect {
+        let response = socks_parser::response::Response {
+            version: socks_parser::Version::Socks5,
+            status: socks_parser::response::Status::CommandNotSupported,
+            addr: request.addr,
+            port: request.port,
+        };
+        buffer.clear();
+        socks_parser::encode_socks_response(&response, &mut buffer);
+        stream.write_all(&buffer[..]).await?;
+        return Ok(());
+    }
+    let host = match request.addr {
+        socks_parser::AddressType::IPv4(ip4) => format!("{}", ip4),
+        socks_parser::AddressType::DomainName(ref n) => n.clone(),
+        socks_parser::AddressType::IPv6(ip6) => format!("[{}]", ip6),
+    };
+
+    let mut sap_stream = match sap_connect(sap_router, &host, request.port).await {
+        Ok(s) => {
+            let response = socks_parser::response::Response {
+                version: socks_parser::Version::Socks5,
+                status: socks_parser::response::Status::Success,
+                addr: request.addr,
+                port: request.port,
+            };
+            buffer.clear();
+            socks_parser::encode_socks_response(&response, &mut buffer);
+            stream.write_all(&buffer[..]).await?;
+            s
+        }
+        Err(e) => {
+            let response = socks_parser::response::Response {
+                version: socks_parser::Version::Socks5,
+                status: socks_parser::response::Status::GeneralFailure,
+                addr: request.addr,
+                port: request.port,
+            };
+            buffer.clear();
+            socks_parser::encode_socks_response(&response, &mut buffer);
+            stream.write_all(&buffer[..]).await?;
+            return Err(e);
+        }
+    };
+
+    tokio::io::copy_bidirectional(&mut stream, &mut sap_stream).await?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let args = Options::parse();
+    dbg!(&args);
+
+    let listener = TcpListener::bind(("127.0.0.1", args.bind_port)).await?;
+
+    loop {
+        let (client, addr) = listener.accept().await?;
+        let sap_router = args.sap_router.clone();
+        tokio::spawn(async move {
+            if let Err(e) = handle_client(sap_router, client).await {
+                eprintln!("Issue with client {addr}: {e}");
+            }
+        });
+    }
+}
---
+++ src/sap.rs
@@ -0,0 +1,470 @@
+use std::{
+    borrow::Cow,
+    io,
+    mem::size_of,
+    net::{SocketAddr, SocketAddrV4, SocketAddrV6},
+};
+
+use nom::{
+    bytes::streaming::{tag, take, take_while},
+    combinator::{map, map_opt, verify},
+    error::{context, ContextError, ErrorKind, VerboseError},
+    multi::{length_data, many_m_n},
+    number::streaming::{be_u16, be_u32, be_u8},
+    sequence::{terminated, tuple},
+};
+use tokio::io::AsyncReadExt;
+
+pub trait Wire<'a>: Sized {
+    fn encode_into(&self, buffer: &mut Vec<u8>);
+    fn decode(input: &'a [u8]) -> nom::IResult<&'a [u8], Self, VerboseError<&'a [u8]>>;
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct SapNi {
+    buffer: Vec<u8>,
+}
+
+impl Default for SapNi {
+    fn default() -> Self {
+        let buffer = 0u32.to_be_bytes().into();
+        Self { buffer }
+    }
+}
+
+impl AsRef<[u8]> for SapNi {
+    fn as_ref(&self) -> &[u8] {
+        &self.buffer[..]
+    }
+}
+
+impl SapNi {
+    pub fn set<'w, W: Wire<'w>>(&mut self, obj: &'w W) {
+        self.buffer.clear();
+        self.buffer.extend_from_slice(&0u32.to_be_bytes()[..]);
+        let offset = self.buffer.len();
+        obj.encode_into(&mut self.buffer);
+        let obj_length = (self.buffer.len() - offset) as u32;
+        self.buffer[..size_of::<u32>()].copy_from_slice(&obj_length.to_be_bytes()[..]);
+    }
+
+    fn get_buffer_len(&self) -> usize {
+        let (_, obj_size) = be_u32::<_, ()>(&self.buffer[..]).unwrap();
+        obj_size as usize
+    }
+
+    pub fn get_data(&self) -> &[u8] {
+        let obj_size = self.get_buffer_len();
+        assert!(obj_size + size_of::<u32>() <= self.buffer.len());
+        &self.buffer[size_of::<u32>()..]
+    }
+
+    pub async fn extract_from_reader<R: AsyncReadExt + Unpin>(
+        &mut self,
+        reader: &mut R,
+    ) -> io::Result<()> {
+        self.buffer.resize(size_of::<u32>(), 0u8);
+
+        reader.read_exact(&mut self.buffer[..]).await?;
+
+        let obj_size = self.get_buffer_len();
+
+        self.buffer.resize(self.buffer.len() + obj_size, 0u8);
+        reader
+            .read_exact(&mut self.buffer[size_of::<u32>()..])
+            .await?;
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct SapRouterHop<'a> {
+    pub hostname: Cow<'a, str>,
+    pub port: Cow<'a, str>,
+    pub password: Cow<'a, str>,
+}
+
+impl<'a> Wire<'a> for SapRouterHop<'a> {
+    fn encode_into(&self, buffer: &mut Vec<u8>) {
+        encode_null_terminated_string(&self.hostname, buffer);
+        encode_null_terminated_string(&self.port, buffer);
+        encode_null_terminated_string(&self.password, buffer);
+    }
+
+    fn decode(input: &'a [u8]) -> nom::IResult<&'a [u8], Self, VerboseError<&'a [u8]>> {
+        context(
+            "SAP Router hop",
+            map(
+                tuple((
+                    decode_null_terminated_string,
+                    decode_null_terminated_string,
+                    decode_null_terminated_string,
+                )),
+                |(hostname, port, password)| Self {
+                    hostname,
+                    port,
+                    password,
+                },
+            ),
+        )(input)
+    }
+}
+
+impl From<SocketAddrV4> for SapRouterHop<'static> {
+    fn from(value: SocketAddrV4) -> Self {
+        let port = format!("{}", value.port());
+        let hostname = format!("{}", value.ip());
+
+        Self {
+            hostname: Cow::Owned(hostname),
+            port: Cow::Owned(port),
+            password: Cow::Owned(String::new()),
+        }
+    }
+}
+
+impl From<SocketAddrV6> for SapRouterHop<'static> {
+    fn from(value: SocketAddrV6) -> Self {
+        let port = format!("{}", value.port());
+        let hostname = format!("[{}]", value.ip());
+
+        Self {
+            hostname: Cow::Owned(hostname),
+            port: Cow::Owned(port),
+            password: Cow::Owned(String::new()),
+        }
+    }
+}
+
+impl From<SocketAddr> for SapRouterHop<'static> {
+    fn from(value: SocketAddr) -> Self {
+        match value {
+            SocketAddr::V4(ip4) => ip4.into(),
+            SocketAddr::V6(ip6) => ip6.into(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[non_exhaustive]
+#[repr(u8)]
+pub enum TalkMode {
+    MsgIo = 0,
+    RawIo = 1,
+    RoutIo = 2,
+}
+
+impl<'a> Wire<'a> for TalkMode {
+    fn encode_into(&self, buffer: &mut Vec<u8>) {
+        buffer.push(*self as u8);
+    }
+
+    fn decode(input: &'a [u8]) -> nom::IResult<&'a [u8], Self, VerboseError<&'a [u8]>> {
+        let (rest, tm) = context("Talk mode", be_u8)(input)?;
+        match tm {
+            0 => Ok((rest, Self::MsgIo)),
+            1 => Ok((rest, Self::RawIo)),
+            2 => Ok((rest, Self::RoutIo)),
+            _ => Err(nom::Err::Failure(nom::error::make_error(
+                input,
+                ErrorKind::NoneOf,
+            ))),
+        }
+    }
+}
+
+const SAP_ROUTER_VERSION: u8 = 2;
+
+#[derive(Debug, PartialEq, Eq)]
+#[non_exhaustive]
+pub enum SapRouter<'a> {
+    Pong,
+    Route {
+        ni_version: u8,
+        talk_mode: TalkMode,
+        rest_nodes: u8,
+        current_position: usize,
+        hops: Vec<SapRouterHop<'a>>,
+    },
+    ErrorInformation {
+        ni_version: u8,
+        operation_code: u8,
+        return_code: u32,
+        text: Cow<'a, str>,
+    },
+}
+
+const SAP_ROUTER_PONG: &str = "NI_PONG";
+const SAP_ROUTER_ROUTE: &str = "NI_ROUTE";
+const SAP_ROUTER_ERROR_INFORMATION: &str = "NI_RTERR";
+
+impl<'a> Wire<'a> for SapRouter<'a> {
+    fn encode_into(&self, buffer: &mut Vec<u8>) {
+        match self {
+            SapRouter::Pong => {
+                encode_null_terminated_string(SAP_ROUTER_PONG, buffer);
+                buffer.push(SAP_ROUTER_VERSION);
+            }
+            SapRouter::Route {
+                ni_version,
+                talk_mode,
+                rest_nodes,
+                current_position,
+                hops,
+            } => {
+                // type
+                encode_null_terminated_string(SAP_ROUTER_ROUTE, buffer);
+                // version
+                buffer.push(SAP_ROUTER_VERSION);
+                // route.ni_version
+                buffer.push(*ni_version);
+                // route.entries
+                buffer.push(hops.len() as u8);
+                // route.talk_mode
+                talk_mode.encode_into(buffer);
+                // unused
+                buffer.extend_from_slice(&0u16.to_be_bytes()[..]);
+                // rest nodes
+                buffer.push(*rest_nodes);
+
+                // route_length
+                let route_length_offset = buffer.len();
+                buffer.extend_from_slice(&0u32.to_be_bytes()[..]);
+
+                // route_offset (current)
+                let route_offset_offset = buffer.len();
+                buffer.extend_from_slice(&0u32.to_be_bytes()[..]);
+                let mut route_offset = 0u32;
+
+                let route_start_offset = buffer.len();
+                for (i, hop) in hops.iter().enumerate() {
+                    if i == *current_position {
+                        route_offset = (buffer.len() - route_start_offset) as u32;
+                    }
+                    hop.encode_into(buffer);
+                }
+
+                let route_length = (buffer.len() - route_start_offset) as u32;
+                (buffer[route_length_offset..][..size_of::<u32>()])
+                    .copy_from_slice(&route_length.to_be_bytes()[..]);
+
+                (buffer[route_offset_offset..][..size_of::<u32>()])
+                    .copy_from_slice(&route_offset.to_be_bytes()[..]);
+            }
+            SapRouter::ErrorInformation {
+                ni_version,
+                operation_code,
+                return_code,
+                text,
+            } => {
+                encode_null_terminated_string(SAP_ROUTER_ERROR_INFORMATION, buffer);
+                buffer.push(SAP_ROUTER_VERSION);
+                buffer.push(*ni_version);
+                buffer.push(*operation_code);
+                buffer.extend_from_slice(&return_code.to_be_bytes()[..]);
+                let text_size = text.len() as u32;
+                buffer.extend_from_slice(&text_size.to_be_bytes()[..]);
+                buffer.extend_from_slice(text.as_bytes());
+            }
+        }
+    }
+
+    fn decode(input: &'a [u8]) -> nom::IResult<&'a [u8], Self, VerboseError<&'a [u8]>> {
+        let (rest, r#type) = context(
+            "SAP Router type",
+            terminated(
+                decode_null_terminated_string,
+                context("version", verify(be_u8, |&v| v == 2)),
+            ),
+        )(input)?;
+
+        match r#type.as_ref() {
+            SAP_ROUTER_PONG => Ok((rest, Self::Pong)),
+            SAP_ROUTER_ROUTE => {
+                let (
+                    rest,
+                    (
+                        ni_version,
+                        entries_count,
+                        talk_mode,
+                        _padd,
+                        rest_nodes,
+                        route_length,
+                        route_offset,
+                    ),
+                ) = context(
+                    "SAP Router Route",
+                    tuple((
+                        be_u8,
+                        be_u8,
+                        TalkMode::decode,
+                        be_u16,
+                        be_u8,
+                        be_u32,
+                        be_u32,
+                    )),
+                )(rest)?;
+
+                if route_offset > route_length {
+                    let e = nom::error::make_error(input, ErrorKind::Verify);
+                    let e = ContextError::add_context(
+                        input,
+                        "route_offset is greater than route_length",
+                        e,
+                    );
+                    return Err(nom::Err::Error(e));
+                }
+
+                let (rest, hops_data) = take(route_length as usize)(rest)?;
+                let (rest_hops, hops) =
+                    many_m_n(1, entries_count as usize, SapRouterHop::decode)(hops_data)?;
+
+                if !rest_hops.is_empty() {
+                    eprintln!("Unparsed data in SapRouterHop: {:?}", rest_hops);
+                }
+
+                if hops.len() != entries_count as usize {
+                    let e = nom::error::make_error(input, ErrorKind::Verify);
+                    let e = ContextError::add_context(
+                        input,
+                        "field `entries_count` does not match hops count",
+                        e,
+                    );
+                    return Err(nom::Err::Error(e));
+                }
+
+                let (_, cur) = SapRouterHop::decode(&hops_data[route_offset as usize..])?;
+                let Some(current_position) = hops.iter().enumerate().find_map(|(i, h)| (h == &cur).then_some(i)) else {
+                    let e = nom::error::make_error(input, ErrorKind::Verify);
+                    let e = ContextError::add_context(
+                        input,
+                        "route_offset does not point to a valid hop",
+                        e,
+                    );
+                    return Err(nom::Err::Error(e));
+                };
+
+                Ok((
+                    rest,
+                    Self::Route {
+                        ni_version,
+                        rest_nodes,
+                        talk_mode,
+                        current_position,
+                        hops,
+                    },
+                ))
+            }
+            SAP_ROUTER_ERROR_INFORMATION => context(
+                "SAP Router Error information",
+                map(
+                    tuple((
+                        be_u8,
+                        terminated(be_u8, be_u8),
+                        be_u32,
+                        map_opt(length_data(be_u32), |d| {
+                            std::str::from_utf8(d).ok().map(|s| s.into())
+                        }),
+                    )),
+                    |(ni_version, operation_code, return_code, text)| Self::ErrorInformation {
+                        ni_version,
+                        operation_code,
+                        return_code,
+                        text,
+                    },
+                ),
+            )(rest),
+            _ => Err(nom::Err::Failure(nom::error::make_error(
+                input,
+                ErrorKind::NoneOf,
+            ))),
+        }
+    }
+}
+
+fn decode_null_terminated_string<'i, E>(input: &'i [u8]) -> nom::IResult<&'i [u8], Cow<'i, str>, E>
+where
+    E: nom::error::ParseError<&'i [u8]> + nom::error::ContextError<&'i [u8]>,
+{
+    context(
+        "Null terminated string",
+        terminated(
+            map_opt(take_while(|b| b != 0), |d| {
+                std::str::from_utf8(d).ok().map(|s| s.into())
+            }),
+            tag(b"\0"),
+        ),
+    )(input)
+}
+
+fn encode_null_terminated_string(s: &str, buffer: &mut Vec<u8>) {
+    buffer.extend_from_slice(s.as_bytes());
+    buffer.push(0);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sap_router_encode() {
+        let router = SapRouter::Route {
+            talk_mode: TalkMode::RawIo,
+            ni_version: 40,
+            rest_nodes: 1,
+            current_position: 1,
+            hops: vec![
+                SapRouterHop {
+                    hostname: "8.8.8.8".into(),
+                    port: "3299".into(),
+                    password: "".into(),
+                },
+                SapRouterHop {
+                    hostname: "10.0.0.1".into(),
+                    port: "3200".into(),
+                    password: "S3cr3t".into(),
+                },
+            ],
+        };
+        let mut buffer = Vec::new();
+        router.encode_into(&mut buffer);
+
+        assert_eq!(
+            &buffer[..],
+            b"NI_ROUTE\x00\x02\x28\x02\x01\x00\x00\x01\
+                   \x00\x00\x00\x23\x00\x00\x00\x0e\
+                   8.8.8.8\x003299\x00\x00\
+                   10.0.0.1\x003200\x00S3cr3t\x00"
+        );
+    }
+
+    #[test]
+    fn sap_router_decode() {
+        let router = SapRouter::Route {
+            talk_mode: TalkMode::RawIo,
+            ni_version: 40,
+            rest_nodes: 1,
+            current_position: 1,
+            hops: vec![
+                SapRouterHop {
+                    hostname: "8.8.8.8".into(),
+                    port: "3299".into(),
+                    password: "".into(),
+                },
+                SapRouterHop {
+                    hostname: "10.0.0.1".into(),
+                    port: "3200".into(),
+                    password: "S3cr3t".into(),
+                },
+            ],
+        };
+        let input = &b"NI_ROUTE\x00\x02\x28\x02\x01\x00\x00\x01\
+                   \x00\x00\x00\x23\x00\x00\x00\x0e\
+                   8.8.8.8\x003299\x00\x00\
+                   10.0.0.1\x003200\x00S3cr3t\x00"[..];
+        let (rest, r) = SapRouter::decode(input).unwrap();
+        assert_eq!(rest.len(), 0);
+        assert_eq!(r, router);
+    }
+}
